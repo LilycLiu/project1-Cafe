9256f4ff05d4743da715c03c73c5881aab5bc98f
Lily Liu <lilycliu@outlook.com>
2023-05-30 08:33:36 +0100

    update

---
+++ restaurant.js
@@ -0,0 +1,9 @@
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+<input type="button" onclick="location.href='https://terryscafe.co.uk/menus/';" value="Go to menu" />
+
